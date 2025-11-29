31c6a59363371fcc435d79ed502135da6779492b
Patryk-Szymula <Patryk7318@gmail.com>
2025-11-29 19:35:56 +0100

    Creating dev environment

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="text-center px-6">
       <h1 className="text-4xl sm:text-5xl font-extrabold tracking-tight">
-        Galaxy Brainer
+        Galaxy Brainer DEV
       </h1>
       <p className="mt-4 text-sm text-zinc-300 max-w-lg mx-auto">
         Your intelligence doesn't have limits!
